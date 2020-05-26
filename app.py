1a4eaefaedfedbc0ec81addf04e50d89aaedee82
Braydan Newman <newmanbraydan@gmail.com>
2020-05-26 22:24:43 +1000

    update

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import render_template, Flask, sessions, request, redirect, url_for
-import requests, json, sqlite3
+import requests
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
-
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 
@@ -16,9 +17,25 @@ login_manager.init_app(app)
 
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True)
-    password = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    truck_owner = db.Column(db.Integer)
+    owner_truck_name = db.Column(db.String)
+    # comment = relationship("Comments")
+    # vote = relationship("Votes")
+    
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    # user_id = db.Column(db.Integer, ForeignKey('User.id'))
+
+
+class Votes(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    # user_id = db.Column(db.Integer, ForeignKey("User.id"))
+    truck_id = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
 
 
 @login_manager.user_loader
@@ -37,10 +54,15 @@ def hello():
     return render_template('login.html')
 
 
-@app.route('/add_user')
+@app.route('/add_user', methods=['POST'])
 def add_user():
     username = request.form['username']
     password = request.form['password']
+
+    user = User.query.filter_by(username=username).first()
+
+    print(user)
+
     user = User(username=username, password=password)
     db.session.add(user)
     db.session.commit()
@@ -49,6 +71,10 @@ def add_user():
 
 @app.route('/login', methods=['POST'])
 def login():
+    selection_type = request.form['type']
+    if selection_type == 'add_user':
+        return render_template('add_user.html')
+
     username = request.form['username']
     password = request.form['password']
     user = User.query.filter_by(username=username).first()
@@ -67,4 +93,6 @@ def sign_up():
 
 
 if __name__ == "__main__":
+    db.create_all()
+    db.session.commit()
     app.run(debug=True)

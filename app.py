52c2a6bdccdc8880c02bd932ef2bef602d04ce9d
Braydan Newman <newmanbraydan@gmail.com>
2020-05-26 18:17:11 +1000

    update

--- app.py
+++ app.py
@@ -1,71 +1,35 @@
-import random
-import string
-
 from flask import render_template, Flask, sessions, request, redirect, url_for
 import requests, json, sqlite3
-from flask_login import LoginManager, UserMixin, login_required, login_user
-
-from SQL_strings import *
-from passlib.hash import sha256_crypt
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
 
 
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-login_manager = LoginManager(app)
-login_manager.login_view = "login"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database\\food_truck.sqlite'
+app.config['SECRET_KEY'] = 'secret_key'
 
 
-class User(UserMixin):
-    Name = ""
-    AlternateID = ""
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-    def get_id(self):
-        return self.AlternateID
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String)
+
 
 @login_manager.user_loader
 def load_user(user_id):
-    db = sqlite3.connect('database/food_truck.sqlite')
-    cursor = db.cursor()
-    cursor.execute(get_user_login_sql_id, (user_id,))
-    result = cursor.fetchall()
-    u = User()
-    u.Name = result[0]
-    u.AlternateID = result[1]
-    return u
+    return User.query.get(int(user_id))
 
 
 def api_data_clean():
     url = "https://www.bnefoodtrucks.com.au/api/1/trucks"
     data = requests.get(url)
     sessions['trucks'] = data.json()
-
-
-def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
-
-
-def valid_login(username, password):
-    success = False
-    try:
-        db = sqlite3.connect('database/food_truck.sqlite')
-        cursor = db.cursor()
-        cursor.execute(get_user_login_sql, (username, ))
-        result = cursor.fetchall()
-
-        if sha256_crypt.verify(password, result):
-            print("P")
-            success = True
-            u = User()
-            u.Name = username
-            u.AlternateID = id_generator().encode('UTF-8', 'strict')
-            login_user(u)
-
-
-    except Exception as error_msg:
-        print(error_msg)
-    finally:
-        db.close()
-    return success
 
 
 @app.route('/')
@@ -76,29 +40,20 @@ def hello():
 @app.route('/add_user')
 def add_user():
     username = request.form['username']
-    ps_hash = pbkdf2_sha256.hash(request.form['password'])
-    try:
-        db = sqlite3.connect('database/food_truck.sqlite')
-        cursor = db.cursor()
-        cursor.execute(add_user_sql, (username, ps_hash))
-    except Exception as error_msg:
-        print(error_msg)
-    finally:
-        db.close()
-
-    return 'Hello, World!'
+    password = request.form['password']
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return render_template("base.html")
 
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/login', methods=['POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return redirect(url_for(main))
-        else:
-            error = 'Invalid username/password'
-    return render_template('login.html', error=error)
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter_by(username=username).first()
+
+    return render_template('base.html')
 
 
 @app.route('/main')
